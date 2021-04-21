bba255ecd9ee9ad826feb2126e7daabf368e25d1
André Kaiser <kaiserkandre@gmail.com>
2021-04-21 12:51:27 +0200

    Fix how students performances are fetched

--- internal/endpoint/performance.go
+++ internal/endpoint/performance.go
@@ -2,7 +2,6 @@ package endpoint
 
 import (
 	"fmt"
-	"reflect"
 	"strings"
 
 	"dhbw-loerrach.de/dualis/microservice/internal/api/models"
@@ -35,21 +34,25 @@ func HandleStudentPerformance(params operations.StudentPerformanceParams) middle
 	enrollments := []*models.Enrollment{}
 	performancesList := models.PerformancesList{}
 
-	filterableValues := map[string]interface{}{"student_fk": params.StudentID, "is_wintersemester": *params.IsWintersemester, "year": *params.Year}
+	filterableValues := map[string]interface{}{"student_fk": params.StudentID}
+
+	if params.IsWintersemester != nil {
+		filterableValues["is_wintersemester"] = *params.IsWintersemester
+	}
+
+	if params.Year != nil {
+		filterableValues["year"] = *params.Year
+	}
+
 	var values []interface{}
 	var where []string
 	for a, b := range filterableValues {
-		if b != nil && !reflect.ValueOf(b).IsNil() {
-			where = append(where, fmt.Sprintf("%s = ?", a))
-			var pVal *interface{}
-			pVal = &b
-			values = append(values, *pVal)
-		}
+		where = append(where, fmt.Sprintf("%s = ?", a))
+		var pVal *interface{}
+		pVal = &b
+		values = append(values, *pVal)
 	}
 
-	fmt.Println("LOOOOOGGG")
-	fmt.Println(values)
-	fmt.Println(where)
 	fmt.Println("SELECT enrollment.id, grade, status, is_wintersemester, year, no, name, credits from enrollment INNER JOIN semester ON enrollment.semester_fk = semester.id INNER JOIN module ON enrollment.module_fk = module.id WHERE " + strings.Join(where, " AND "))
 
 	rows, err := db.Query("SELECT enrollment.id, grade, status, is_wintersemester, year, no, name, credits from enrollment INNER JOIN semester ON enrollment.semester_fk = semester.id INNER JOIN module ON enrollment.module_fk = module.id WHERE "+strings.Join(where, " AND "), values...)
@@ -105,6 +108,7 @@ func HandleStudentPerformance(params operations.StudentPerformanceParams) middle
 		moduleNumberVal := moduleNumber
 
 		moduleResults = append(moduleResults, &models.ModuleResult{Credits: &moduleCreditsVal, LectureResults: lectureResults, Name: &moduleNameVal, Number: &moduleNumberVal})
+		lectureResults = nil
 
 		enrollmentGradeVal := enrollmentGrade
 		enrollmentStatusVal := enrollmentStatus
@@ -120,7 +124,7 @@ func HandleStudentPerformance(params operations.StudentPerformanceParams) middle
 		}
 
 		enrollments = append(enrollments, &models.Enrollment{Grade: enrollmentGradeVal, ModuleResult: moduleResults, Semester: &semester, Status: &enrollmentStatusVal})
-
+		moduleResults = nil
 	}
 
 	performancesList = models.PerformancesList{Enrollments: enrollments}
